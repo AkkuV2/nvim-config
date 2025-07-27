dc115f3ed28a46c4841e66dc7da5ecb9790aa737
akku <el.aliherazo@gmail.com>
2025-07-26 23:51:18 -0500

    config update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrw = 1
+
+vim.opt.tabstop = 4 --Número de espacios que equivale una tabulación
+vim.opt.shiftwidth = 4 --Número de espacios que se usan al hacer identado (>> o <<)
+vim.opt.expandtab = true --Convierte tabs en espacios reales
